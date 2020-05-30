5f46e75d52966481c19ca19081892ff9b2c17990
Efimov Vasily <laer.18@gmail.com>
2020-05-30 18:09:27 +0300

    LocalsCatcher.__str__: support recursion
    Signed-off-by: Efimov Vasily <laer.18@gmail.com>

--- common/localscatcher.py
+++ common/localscatcher.py
@@ -47,11 +47,25 @@ to `self`.
                 setattr(self, l, locs[l])
 
     def __str__(self):
-        return super(LocalsCatcher, self).__str__() + \
-            ":\n    " + \
-            "\n    ".join(
-                (n + " = " + "\n    ".join(str(v).split("\n"))
-            ) for n, v in self.__dict__.items())
+        res_lines = [super(LocalsCatcher, self).__str__()]
+
+        visited = set()
+        for n, v in self.__dict__.items():
+            vid = id(v)
+            if vid in visited:
+                v_lines = [n + " = # recursion #"]
+            else:
+                visited.add(vid)
+                v_lines = str(v).split("\n")
+                if v_lines:
+                    v_lines[0] = n + " = " + v_lines[0]
+                else:
+                    v_lines.append(n + ' = ""')
+
+            v_indented_lines = "\n    ".join(v_lines)
+            res_lines.append(v_indented_lines)
+
+        return "\n    ".join(res_lines)
 
     def catch_locals(self):
         for k, v in _getframe(1).f_locals.items():
